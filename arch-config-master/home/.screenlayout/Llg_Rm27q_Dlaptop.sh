aa222489a06830c451a44b99a750fdacf5f3f6c9
arkeyve <arkeyve@gmail.com>
2022-02-02 00:58:49 +0530

    m27q primary monitor

---
+++ arch-config-master/home/.screenlayout/Llg_Rm27q_Dlaptop.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+xrandr --output DP-0 --off --output DP-1 --off --output HDMI-0 --mode 1920x1080 --pos 0x0 --rotate right --output DP-2 --primary --mode 2560x1440 --pos 1080x240 --rotate normal --output DP-3 --off --output eDP-1-0 --mode 1920x1080 --pos 1400x1680 --rotate normal
